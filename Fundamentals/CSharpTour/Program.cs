200e054816598267f8f7803bf3af51c5126cbf77
Vitor <victor-bash2@hotmail.com>
2025-01-27 22:13:16 -0300

    add C# tour first code

---
+++ Fundamentals/CSharpTour/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// This is the new feature called top level statement
+// Usings are implicit here
+Console.WriteLine("Hello World from a top level statement.");
+
+// Below is the old way of declaring an application entry point
+// Entry point stands for initial code to be executed
+//using System;
+
+//public static class Program
+//{
+//    public static void Main()
+//    {
+//        Console.WriteLine("Hello World from Main method.");
+//    }
+//}
